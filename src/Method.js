432405cced613fe68313e315c2c236dea7344352
NickLatkovich <nicklatkovich@gmail.com>
2018-12-28 16:01:08 +0300

    Tests of Method

--- src/Method.js
+++ src/Method.js
@@ -1,4 +1,6 @@
+import { Echo } from 'echojs-lib';
 import decode from './decoders';
+import { checkContractId } from './utils/validators';
 
 /** @typedef {import("./Contract").default} Contract */
 /** @typedef {import("../types/_Abi").AbiArgument} AbiArgument */
@@ -46,18 +48,20 @@ export default class Method {
 		} else checkContractId(contractId);
 		if (assetId === undefined) assetId = '1.3.0';
 		else if (!/^1\.3\.(0|[1-9]\d*)$/.test(assetId)) throw new Error('invalid assetId format');
-		if (accountId === undefined) accountId = '1.2.0';
+		if (accountId === undefined) accountId = '1.2.1';
 		else if (!/^1\.2\.(0|[1-9]\d*)$/.test(accountId)) throw new Error('invalid accountId format');
-		echo = echo || this._contract.echo;
-		if (!echo) throw new Error('no echo instance');
+		if (echo === undefined) {
+			if (this._contract.echo === undefined) throw new Error('no echo instance');
+			echo = this._contract.echo;
+		} else if (!echo instanceof Echo) throw new Error('invalid echo instance');
 		// FIXME: remove @type when JSDoc of callContractNoChangingState will be fixed
 		/** @type {string} */
 		const rawResult = await echo.api.callContractNoChangingState(contractId, accountId, assetId, this.code);
 		if (rawResult === '') {
-			if (_abiMethodOutputs.length === 0) return null;
+			if (this._abiMethodOutputs.length === 0) return null;
 			throw new Error('call failed');
 		}
-		return decode(rawResult, _abiMethodOutputs.outputs.map(({ type }) => type));
+		return decode(rawResult, this._abiMethodOutputs.map(({ type }) => type));
 	}
 
 }
