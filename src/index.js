fa7ced9297c91793804e23bbd60176a8b56f56e5
Demid Mosko <d.mosko@pixelplex.by>
2019-03-13 15:49:37 +0300

    Export decode

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import * as EchoJSLib from 'echojs-lib';
 export default Contract;
 export { PrivateKey, default as echo, Echo, BigNumber } from 'echojs-lib';
 export { default as encode } from './encoders';
+export { default as decode } from './decoders';
 export { generateInterface, EchoJSLib };
